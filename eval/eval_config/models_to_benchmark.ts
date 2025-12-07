988c187161ced5d6c6dfc83f57d897a81caa6ffd
dagmawibabi <1babidagi@gmail.com>
2025-12-07 05:41:21 +0300

    better model runs

--- eval/eval_config/models_to_benchmark.ts
+++ eval/eval_config/models_to_benchmark.ts
@@ -1,14 +1,14 @@
 export const modelsToBenchmark = [
   { name: "gpt-oss-20b", input: "openai/gpt-oss-20b" },
-  // { name: "trinity-mini", input: "arcee-ai/trinity-mini" },
-  // { name: "deepseek-v3.2", input: "deepseek/deepseek-v3.2" },
-  // { name: "llama-3.1-8b", input: "meta/llama-3.1-8b" },
-  // { name: "nova-lite", input: "amazon/nova-lite" },
-  // { name: "grok-4-fast-non-reasoning", input: "xai/grok-4-fast-non-reasoning" },
-  // {
-  //   name: "gemini-2.5-flash-lite",
-  //   input: "google/gemini-2.5-flash-lite",
-  // },
-  // { name: "gemini-2.5-flash", input: "google/gemini-2.5-flash" },
-  // { name: "gemini-2.0-flash-lite", input: "google/gemini-2.0-flash-lite" },
+  { name: "trinity-mini", input: "arcee-ai/trinity-mini" },
+  { name: "deepseek-v3.2", input: "deepseek/deepseek-v3.2" },
+  { name: "llama-3.1-8b", input: "meta/llama-3.1-8b" },
+  { name: "nova-lite", input: "amazon/nova-lite" },
+  { name: "grok-4-fast-non-reasoning", input: "xai/grok-4-fast-non-reasoning" },
+  {
+    name: "gemini-2.5-flash-lite",
+    input: "google/gemini-2.5-flash-lite",
+  },
+  { name: "gemini-2.5-flash", input: "google/gemini-2.5-flash" },
+  { name: "gemini-2.0-flash-lite", input: "google/gemini-2.0-flash-lite" },
 ];
